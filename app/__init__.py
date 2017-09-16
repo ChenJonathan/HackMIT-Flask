e29850c2f46d0b5db1e0349e61385d0f518175f7
ndass6 <ND@USComputers.net>
2017-09-16 15:32:02 -0400

    Added MySQL integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,41 @@
 from flask import Flask, render_template, jsonify, request, g, redirect, url_for
+from flask.ext.mysql import MySQL
+import os
 
 app = Flask(__name__)
+
+# Get the proper config object
+if os.environ.get('TYPE') == 'production':
+    app.config.from_object('config.ProductionConfig')
+else:
+    app.config.from_object('config.DevelopmentConfig')
+
+mysql = MySQL()
+mysql.init_app(app)
+
+def getDB():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'mysql_db'):
+        g.mysql_db = mysql.connect()
+    
+    return g.mysql_db
+
+def getCursor():
+    if not hasattr(g, 'mysql_db'):
+        g.mysql_db = mysql.connect()
+
+    if not hasattr(g, 'cursor'):
+        g.cursor = g.mysql_db.cursor()
+
+    return g.cursor
+
+@app.teardown_appcontext
+def closeDB(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'mysql_db'):
+        g.mysql_db.close()
 
 @app.route("/")
 def home():
